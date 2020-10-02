4291d65060295005f87d02017884f83fa58e4a5c
Henry Zeng <z13405868569@163.com>
2020-10-03 07:02:14 +0800

    环境信息获取

--- Environment.py
+++ Environment.py
@@ -4,6 +4,12 @@ import os
 path = Workpath = os.path.dirname(os.path.abspath(__file__))
 # 工程根目录
 
+if os.name == 'nt':
+    Workpath = path.split('\\')
+else:
+    Workpath = path.split('/')
+# 工作目录分割
+
 next_ = None
 if os.name == 'nt':
     next_ = '\\'
@@ -12,16 +18,13 @@ else:
 # 针对 Linux 和 windows 的不同，更改路径中的 反斜杠(\) 或 斜杠(/)
 
 def dir_mix(*dirs):
-    mix = ''
-    for i in dirs:
-        mix += i+next_
-    return mix[:-1]
+    return next_.join(dirs)
 # 路径拼合函数
 
-
 def path_read(path):
-    child_path = ''
-    for i in path:
-        child_path += i + next_
-    return child_path[:-1]
+    return next_.join(path)
 # 读取path数据，合成子路径
+
+root = path_read(Workpath[:-1])
+
+Package_path = path_read([root,"Package"]) # Package 扫描目录
