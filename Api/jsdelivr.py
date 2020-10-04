a4a6fcb7562eda3afc2b8cbfd5c90532115eeb74
Henry Zeng <z13405868569@163.com>
2020-10-04 20:35:25 +0800

    仓库数据获取和下载

--- Api/jsdelivr.py
+++ Api/jsdelivr.py
@@ -1,29 +1,52 @@
+# -*- coding: utf-8 -*-
 import requests,os,json
+from PySide2 import QtCore
 
 
 class jsdelivr():
-    def __init__(self, package):
+    def __init__(self, package,error):
         self.base = 'https://cdn.jsdelivr.net/gh/Cardinal-Designer/PackageCube/'
         self.root = self.base + package
         self.download = 'https://cdn.jsdelivr.net/gh/'
         self.download_url = "https://cdn.jsdelivr.net/gh/Cardinal-Designer/" + package
+        self.DownloadUnit = Download_Unit(package)
+        self.DownloadUnit.error.connect(error)
 
     def tags(self):
         try:
-            return json.loads(requests.get(self.root + '/' + 'Origin.json').text)["versions"]
+            return json.loads(requests.get(self.root + '/' + 'Origin.json',timeout = 3).text)["versions"]
         except:
             return ValueError
 
     def version(self,Tag):
         try:
-            return json.loads(requests.get(self.root + '/' + Tag +'.json').text)
+            return json.loads(requests.get(self.root + '/' + Tag +'.json',timeout = 3).text)
         except:
-            pass
+            return ValueError
+
+class Download_Unit(QtCore.QThread):
+    end = QtCore.Signal()
+    error = QtCore.Signal(str)
+    def __init__(self,package):
+        super().__init__()
+        self.download_url = "https://cdn.jsdelivr.net/gh/Cardinal-Designer/" + package
+        self.key = False
 
     def PackDownload(self, Object, Tag, path, callback=None):
+        if self.key:
+            self.error.emit("心急吃不了热豆腐！")
+            return
+        self.Object = Object
+        self.Tag = Tag
+        self.path = path
+        self.callback = callback
+        self.start()
+
+    def run(self):
+        self.key = True
         Pass_directory = [".idea"]
 
-        work = Object["files"]
+        work = self.Object["files"]
         if os.name == 'nt':
             Replace = '\\'
         else:
@@ -39,18 +62,20 @@ class jsdelivr():
                         continue
 
                     try:
-                        os.makedirs(path + Path.replace('/', Replace))
+                        os.makedirs(self.path + Path.replace('/', Replace))
                     except:
                         pass
 
                     find(Path, p["files"])
                 if p["type"] == "file":
-                    url = self.download_url + "@" + Tag + Path
-                    if not callback == None:
-                        callback(url)
+                    url = self.download_url + "@" + self.Tag + Path
+                    if not self.callback == None:
+                        self.callback(url)
 
-                    with open(path + Path.replace('/', Replace), 'wb+') as f:
+                    with open(self.path + Path.replace('/', Replace), 'wb+') as f:
                         req = requests.get(url).content
                         f.write(req)
 
         find('', work)
+        self.key = False
+        self.end.emit()
