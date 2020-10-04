5d5551cd10a23e501654310a0e0d8bdd7517f232
Henry Zeng <z13405868569@163.com>
2020-10-04 20:40:21 +0800

    第一级窗口的入口类

--- Quick.py
+++ Quick.py
@@ -1,25 +1,18 @@
 # -*- coding:utf-8 -*-
 from PySide2 import QtWidgets, QtCore
 from UI.Mainwindow import Ui_MainWindow
-from Environment import Package_path, dir_mix
+from Environment import Package_path, dir_mix,wrapper_path,Player_path
 from DataUnCopy import Space, Add
 from PySide2.QtCore import Signal
 from Add import Add_windows
 from Card import PersonCard
+from config import config
+import subprocess
 import math, json, os
 
-def message_info(type, title, msgs):
-    msg = {
-        'type': type,
-        'title': title,
-        'msg': msgs
-    }
-    return json.dumps(msg)
-    # 包装message数据
-
-
 class ReadConfig(QtCore.QThread):
-    Create = Signal(str,str)
+    Create = Signal(str)
+    error = Signal(str)
     def __init__(self):
         super(ReadConfig, self).__init__()
 
@@ -33,6 +26,7 @@ class ReadConfig(QtCore.QThread):
             break
 
         now_Process = None
+
         try:
             for pkg_name in Child_path:
                 now_Process = pkg_name
@@ -44,12 +38,20 @@ class ReadConfig(QtCore.QThread):
                 # 合并多段数据 组成config文件的目录数据
 
                 with open(config_file, 'r', encoding='utf-8') as f:
-                    Package_root = dir_mix(Path,pkg_name)
-                    cover =  dir_mix(Package_root,json.loads(f.read())["cover"])
-                    self.Create.emit(Package_root,cover)
+                    content = json.loads(f.read())
+                    Package_root = dir_mix(Path, pkg_name)
+
+                    To = {
+                        "Package_root" : Package_root,
+                        "cover" : dir_mix(Package_root, content["cover"]),
+                        "name" : content["Name"],
+                        "Description" : content["Description"]
+                    }
+
+                    self.Create.emit(json.dumps(To))
 
         except:
-            self.message.emit(message_info(type='wrong', title='error', msgs='包：[' + now_Process + ']异常'))
+            self.error.emit('包：[' + now_Process + ']异常')
             # 回调，发送出错的包的名称
 
 
@@ -64,22 +66,27 @@ class Quick(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setWindowTitle("QuickLauncher")
         self.N = 0  # 初始为0，在第一次调用Create时候会加1
 
-        # Add('configs')
-        # Space['configs'] = []
-        # Add('pkg_root')
-        # Space['pkg_root'] = []
-        # 创建跨组件共享变量
+        Add('select')
+        Space['select'] = set()
+        #创建跨组件共享变量
 
         self.Card = {}
-        self.Card_List = []
+        self.Card_List = {}
 
     def show(self) -> None:
         super().show()
         self.FindPackage = ReadConfig()
+        self.FindPackage.error.connect(self.error)
         self.FindPackage.Create.connect(self.Create)
         self.FindPackage.start()
 
-    def Create(self,Package_root,Icon):
+    def error(self,text):
+        QtWidgets.QMessageBox.warning(self, 'error', text, QtWidgets.QMessageBox.Yes)
+
+    def Create(self,content):
+        receive = json.loads(content)
+        Package_root = receive["Package_root"]
+        Icon = receive["cover"]
         self.N += 1
 
         w = (self.N - 1) % 5
@@ -90,16 +97,30 @@ class Quick(QtWidgets.QMainWindow, Ui_MainWindow):
         y = (l > 0) * (l - 1) * 150 + 6 * (l > 0) * (l - 1)
 
         hash_ = hash(Package_root)
-
-        self.Card[hash_] = PersonCard(self.scrollAreaWidgetContents, x, y,Icon)
+        self.Card_List[hash_] = Package_root
+        self.Card[hash_] = PersonCard(self.scrollAreaWidgetContents, x, y,Icon,hash_,receive)
         self.Card[hash_].show()
 
         l = math.ceil(self.N / 5)
         L = (l > 0) * (l - 1) * 150 + 6 * (l > 0) * (l - 1) + 300
         self.scrollAreaWidgetContents.setMinimumSize(QtCore.QSize(780, L))
 
+
+
     def AddDepends(self):
         self.Add_window.show()
 
     def RunAll(self):
-        pass
+        v = config.config['PlayerVersion']
+        if v == 'Wrong version' or not v:
+            QtWidgets.QMessageBox.warning(self, 'error', '没有找到show组件，请点击右下角加号图包进行安装', QtWidgets.QMessageBox.Yes)
+        elif not self.Card_List:
+            QtWidgets.QMessageBox.warning(self, 'error', '没有图包，自行添加', QtWidgets.QMessageBox.Yes)
+        elif not Space['select']:
+            QtWidgets.QMessageBox.warning(self, 'error', '没有选择图包', QtWidgets.QMessageBox.Yes)
+        else:
+            choose = QtWidgets.QMessageBox.information(self, 'Success', '一切就绪', QtWidgets.QMessageBox.No,QtWidgets.QMessageBox.Yes)
+            if choose == QtWidgets.QMessageBox.StandardButton.Yes:
+                for i in Space['select']:
+                    subprocess.Popen([wrapper_path,dir_mix(Player_path,v),self.Card_List[i]])
+
